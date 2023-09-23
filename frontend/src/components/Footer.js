85a4c5cb3804b0f6f9da69026fe665527074d035
Richie Thakkar <richiethakkar@gmail.com>
2023-09-23 16:42:13 +0530

    Minor improvements and reuirements file updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer()
     return(
         <div className="footer">
         <span>
-        Made with <span role="img" aria-label="heart emoji">❤️</span> by <a href="https://www.linkedin.com/in/richie-thakkar-5a722a206/" target="_blank" style={linkstyle}>Richie</a> using REACT 
+        Made with <span role="img" aria-label="heart emoji">❤️</span> by <a href="https://www.linkedin.com/in/richie-thakkar-5a722a206/" target="_blank" style={linkstyle}>Richie, Reshma and Bhuvanesh</a> using REACT 
         </span>
       </div>
         )
